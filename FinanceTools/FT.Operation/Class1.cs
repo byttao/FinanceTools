a8c1345d480f2af3e1837757eb26c7c4b6feed29
白鷐 <byttao@gmail.com>
2017-12-08 00:29:15 +0800

    按键按钮

---
+++ FinanceTools/FT.Operation/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FT.Operation
+{
+    public class Class1
+    {
+    }
+}
---
+++ FinanceTools/FinanceTools/FTRibbon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace FinanceTools
+{
+    public partial class FTRibbon
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void BtnPayroll_Click(object sender, RibbonControlEventArgs e)
+        {
+
+        }
+    }
+}
